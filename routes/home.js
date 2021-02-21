3a1fe05794c88bbd945d6f18c0eea0ed3f53092d
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2021-02-21 20:42:50 +0530

    some shitty changes to the home page. Absolutely unnecessary

--- routes/home.js
+++ routes/home.js
@@ -8,12 +8,16 @@ router.get('/', (req, res) => {
     <h3 style="text-align: center; font-family: Helvetica;margin:0px;font-weight:200; margin-top: 30px;">
     This is the home page of the warehouse API(Application Programming Interface).<br>Some of the routes are.
     </h3>
-    <h3 style="text-align: center; font-family: Helvetica;margin:0px;font-weight:400; margin-top: 20px"><i>
-    GET     - minorapi.ddns.net/products/:id<br>
-    POST    - minoapi.ddns.net/products<br>
-    PUT     - minoapi.ddns.net/products<br>
-    DELETE  - minoapi.ddns.net/products
-    </i></h3>
+    <h3 style="font-family: Helvetica;margin:0px;font-weight:400; margin-top: 20px">
+    <div style="text-align: left;">
+    <i>
+    GET     - /products/:id<br>
+    POST    - /products<br>
+    PUT     - /products<br>
+    DELETE  - /products
+    </i>
+    </div>
+    </h3>
     `)
 });
    
